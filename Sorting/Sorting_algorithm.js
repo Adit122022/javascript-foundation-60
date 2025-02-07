545b62092ec28c10a0e679f6a78c030a2f1a6654
Adit122022 <adity202212@gmail.com>
2025-02-07 11:41:52 +0530

    insertion sort

--- Sorting/Sorting_algorithm.js
+++ Sorting/Sorting_algorithm.js
@@ -1,10 +1,15 @@
 /* 
 INSERTION SORT
-*/
+[ 7  ,  4  ,  3  ,  8  ,  9  ]
+[ 4  ,  7  ,  3  ,  8  ,  9  ]
+[ 4  ,  4  ,  7  ,  8  ,  9  ]
+[ 3  ,  4  ,  7  ,  8  ,  9  ]
+
+
 let arr =[7,4,3,5,8,9]
 for(let i=1;i<arr.length;i++){
     let key = arr[i]
-    let j = i-1
+    let j = i-1    
     while(j>=0 && arr[j] > key){
         arr[j+1] = arr[j]
         j--;
@@ -12,3 +17,4 @@ for(let i=1;i<arr.length;i++){
     arr[j+1] = key
 }
 console.log(arr)
+*/
